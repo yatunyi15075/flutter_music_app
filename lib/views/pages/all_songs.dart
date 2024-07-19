68c735177220281d5885c0173a317e365b4a9a77
YATUNYI BRIAN ONDEKO <107187750+yatunyi15075@users.noreply.github.com>
2024-07-19 07:08:52 +0300

    Update all_songs.dart
    Converted AllSongsPage from StatelessWidget to StatefulWidget: This allows for fetching and updating data asynchronously.
    
    Added isLoading state: To show a loading indicator while data is being fetched.
    
    Added _fetchSongs method: This method uses ShazamService to fetch songs from the Shazam API and updates the state with the fetched songs.
    
    Modified the build method: It now shows a loading indicator when fetching data and displays the songs once they are loaded.

--- lib/views/pages/all_songs.dart
+++ lib/views/pages/all_songs.dart
@@ -1,18 +1,36 @@
 import 'dart:math';
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
+import 'shazam_service.dart';
 
-class AllSongsPage extends StatelessWidget {
-  final List<Song> songs = List.generate(
-    20,
-    (index) => Song(
-      title: 'Song Title $index',
-      artist: 'Artist $index',
-      album: 'Album $index',
-      coverUrl: 'https://picsum.photos/200?random=$index',
-    ),
-  );
+class AllSongsPage extends StatefulWidget {
+  @override
+  _AllSongsPageState createState() => _AllSongsPageState();
+}
+
+class _AllSongsPageState extends State<AllSongsPage> {
+  List<Song> songs = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchSongs();
+  }
+
+  Future<void> _fetchSongs() async {
+    try {
+      final fetchedSongs = await ShazamService.fetchSongs('metallica');
+      setState(() {
+        songs = fetchedSongs;
+        isLoading = false;
+      });
+    } catch (e) {
+      setState(() {
+        isLoading = false;
+      });
+      // Handle error
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,58 +41,46 @@ class AllSongsPage extends StatelessWidget {
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
-      body: Column(
-        children: [
-          Container(
-            height: 200,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: NetworkImage(
-                  'https://picsum.photos/800/400?random=${Random().nextInt(100)}',
+      body: isLoading
+          ? Center(child: CircularProgressIndicator())
+          : Column(
+              children: [
+                Container(
+                  height: 200,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage(
+                        'https://picsum.photos/800/400?random=${Random().nextInt(100)}',
+                      ),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  child: Center(
+                    child: Text(
+                      '${songs.length} songs',
+                      style: TextStyle(color: Colors.white, fontSize: 24),
+                    ),
+                  ),
                 ),
-                fit: BoxFit.cover,
-              ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: songs.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        leading: Image.network(songs[index].coverUrl,
+                            width: 50, height: 50),
+                        title: Text(songs[index].title,
+                            style: TextStyle(color: Colors.white)),
+                        subtitle: Text(
+                            '${songs[index].artist} - ${songs[index].album}',
+                            style: TextStyle(color: Colors.grey)),
+                        trailing: Icon(Icons.more_vert, color: Colors.white),
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
-            child: Center(
-              child: Text(
-                '${songs.length} songs',
-                style: TextStyle(color: Colors.white, fontSize: 24),
-              ),
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: songs.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  leading: Image.network(songs[index].coverUrl,
-                      width: 50, height: 50),
-                  title: Text(songs[index].title,
-                      style: TextStyle(color: Colors.white)),
-                  subtitle: Text(
-                      '${songs[index].artist} - ${songs[index].album}',
-                      style: TextStyle(color: Colors.grey)),
-                  trailing: Icon(Icons.more_vert, color: Colors.white),
-                );
-              },
-            ),
-          ),
-        ],
-      ),
     );
   }
-}
-
-class Song {
-  final String title;
-  final String artist;
-  final String album;
-  final String coverUrl;
-
-  Song({
-    required this.title,
-    required this.artist,
-    required this.album,
-    required this.coverUrl,
-  });
 }
